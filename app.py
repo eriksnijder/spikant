3a2b76a6a9149ee0bb5c7866d9311a220ed5eeb8
Erik Snijder <erik.snijder@cloudnation.nl>
2023-06-15 17:33:00 +0200

    alle banen toegevoegd

--- app.py
+++ app.py
@@ -44,6 +44,46 @@ def index():
         'rows8': [
             ['COG', 342,81,108,304,124,304,124,304,124,304,124,304,243],
             ['TWA', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # Empty for calculation
+        ],
+        'columns9': ['Z kort', '60s', '54s', 'Db', 'Bs', 'Cs', 'Bs', 'Cs', 'Ab', 'Db', 'Ab', 'Finish'],
+        'rows9': [
+            ['COG', 342,88,124,40,175,355,175,304,166,346,243],
+            ['TWA', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # Empty for calculation
+        ],
+        'columns10': ['Z lang', '60s', '54s', 'Db', 'Bs', 'Cs', 'Ab', 'Db', 'Bs', 'Cs', 'Ab', 'Db', 'Bs', 'Cs', 'Ab', 'Finish'],
+        'rows10': [
+            ['COG', 342,88,124,40,175,304,166,40,175,304,166,40,175,304,243],
+            ['TWA', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # Empty for calculation
+        ],
+        'columns11': ['ZW kort', '60s', '47s', 'Db', 'Bb', 'Db', 'Bb', 'Db', 'Bb', 'Finish'],
+        'rows11': [
+            ['COG', 342,81,196,40,220,40,220,40,255],
+            ['TWA', 0, 0, 0, 0, 0, 0, 0, 0, 0] # Empty for calculation
+        ],
+        'columns12': ['ZW lang', '60s', '47s', 'Db', 'Bb', 'Db', 'Bb', 'Db', 'Bb', 'Db', 'Bb', 'Finish'],
+        'rows12': [
+            ['COG', 342,81,196,40,220,40,220,40,220,40,255],
+            ['TWA', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # Empty for calculation
+        ],
+        'columns13': ['W kort', '60s', '48b', '47b', '48b', 'Finish'],
+        'rows13': [
+            ['COG', 342,81,267,40,265],
+            ['TWA', 0, 0, 0, 0, 0] # Empty for calculation
+        ],
+        'columns14': ['W lang', '60s', '46b', '45b', '46b', '45b', '46b', 'Finish'],
+        'rows14': [
+            ['COG', 342,83,267,87,267,87,264],
+            ['TWA', 0, 0, 0, 0, 0, 0, 0] # Empty for calculation
+        ],
+        'columns15': ['NW kort', '60s', '54s', 'Cb', 'Ab', 'Cb', 'Ab', 'Cb', '47b', 'Finish'],
+        'rows15': [
+            ['COG', 342,88,102,304,124,302,124,315,259],
+            ['TWA', 0, 0, 0, 0, 0, 0, 0, 0, 0] # Empty for calculation
+        ],
+        'columns16': ['NW lang', '60s', '54s', 'Cb', 'Ab', 'Cb', 'Ab', 'Cb', 'Ab', 'Cb', 'Ab', 'Cb', '47b', 'Finish'],
+        'rows16': [
+            ['COG', 342,88,102,304,124,302,124,302,124,302,124,315,259],
+            ['TWA', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] # Empty for calculation
         ]
     }
     return render_template('table.html', **data)
